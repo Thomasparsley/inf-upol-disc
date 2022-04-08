a7742dd129abd5f6cc33add4cf5cfffb6d4b5054
Marek Brodacký <brodackym@gmail.com>
2022-04-08 12:58:04 +0200

    Command register
    Co-Authored-By: Tomáš Petržela <info@thomasparsley.cz>

--- bot/src/commands/commandRegister.ts
+++ bot/src/commands/commandRegister.ts
@@ -2,19 +2,11 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 import { Command } from "../command";
 
-const RequiredOptionEmail = "argemail";
-
 export const commandRegister = new Command(
     "cmdreg",
     "Zaregistruje command, který je součástí S2A2 botu. WIP.",
-    new SlashCommandBuilder()
-    .addStringOption(option => {
-        return option
-            .setName(RequiredOptionEmail)
-            .setDescription("Zadejte validní email.")
-            .setRequired(true);
-    }),
+    new SlashCommandBuilder(),
     async ({ interaction }) => {
-        await interaction.reply("WIP");
+        
     },
 );
