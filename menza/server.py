dd8b5957354960437f92d92973442450aab0352d
Tomáš Petržela <info@thomasparsley.cz>
2022-04-16 12:18:03 +0200

    get one day

--- menza/server.py
+++ menza/server.py
@@ -1,7 +1,40 @@
-from flask import Flask
+import datetime
+import pathlib
+import os
 
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from svickova.engine import download_menu
+from svickova.enums import Canteen
+
+app = FastAPI()
+
+chromer_driver_path = os.getcwd() + "/chromedriver"
+os.chmod(chromer_driver_path, 0o777)
+
+options = Options()
+options.headless = True
+service = Service(pathlib.Path(chromer_driver_path))
+driver = webdriver.Chrome(service=service, options=options)
+
+@app.get("/")
+async def hello_world():
+    menu_today = download_menu(
+        Canteen.LISTOPAD_17,
+        datetime.date.today() + datetime.timedelta(days=5),
+        driver,
+    )
+
+    menu_today_json = []
+
+    for meal in menu_today:
+        menu_today_json.append(meal.to_json())
+
+    return {
+        "menu": menu_today_json,
+        "items": len(menu_today),
+    }
