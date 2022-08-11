20d4c02fb6a1b435bb843420db2ba425cb305ce4
slunimara <brodackym@gmail.com>
2022-08-11 10:33:34 +0200

    Update

--- bot/src/commands/register.ts
+++ bot/src/commands/register.ts
@@ -19,17 +19,22 @@ export const commandRegister = new Command(
                 .setRequired(true);
         }),
     async ({ interaction, replySilent, permissionRolesCount, mailer }) => {
-        const hasPermission = permissionRolesCount((size: Number) => size === 1);
-        if (!hasPermission)
-            throw new UnauthorizedError();
 
+        // mohou použít jen nový uživatelé, kteří nejsou zvalidováni
+        // const hasPermission = permissionRolesCount((size: Number) => size === 1);
+        // if (!hasPermission)
+        //     throw new UnauthorizedError();
+        
         const email = interaction.options.getString(cd.options[0].name);
+        // validace emailu
         if (email === null || !isValidateEmail(email))
             throw new InvalidEmailFormatError(email as string);
 
+        // validace domény emailu    
         if (!isUpolEmail(email))
             throw new UnknownUpolEmailError(email);
 
+        // vygenerování 6 místného klíče   
         const verificationCode = Math.floor(Math.random() * 900000) + 100000;
 
         console.log(`Nové vygenerované heslo je: ${verificationCode}`)
