195364c02650eca87d10d7364dfffb29b1412ba7
Tomáš Petržela <info@thomasparsley.cz>
2022-08-16 12:28:15 +0200

    Student verification update
    Co-authored-by: Marek Brodacký <slunimara@users.noreply.github.com>

--- bot/src/commands/buttons/verification.ts
+++ bot/src/commands/buttons/verification.ts
@@ -1,23 +1,29 @@
 import { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
 import { ButtonCommand } from "../../command";
+import { Roles } from "../../enums";
 
 
 const modalRow = new ActionRowBuilder()
     .addComponents(
         new TextInputBuilder()
-            .setCustomId("customId")
+            .setCustomId("verificationStudentUpolEmail")
             .setLabel("Zadejte Váš studentský email v rámci UPOL")
             .setStyle(TextInputStyle.Short),
     ) as ActionRowBuilder<TextInputBuilder>;
 const modal = new ModalBuilder()
-    .setCustomId("customId")
+    .setCustomId("verificationStudentModal")
     .setTitle("Verifikace studenta")
     .addComponents(modalRow);
 
 export const verificationFirewallButtonComamand = new ButtonCommand(
     "btnStuden",
     "Vrátí formulář pro verifikaci studenta",
-    async ({ interaction }) => {
+    async ({ interaction, hasRole, replySilent }) => {
+        if (hasRole(Roles["Student"])) {
+            await replySilent("Již jste oveřený.");
+            return;
+        }
+
         await interaction.showModal(modal);
     },
 );
