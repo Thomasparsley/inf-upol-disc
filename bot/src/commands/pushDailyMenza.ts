8e6f5ac6f513e1541adda163b62093a4e05b5721
Tomáš Petržela <info@thomasparsley.cz>
2022-09-27 06:15:07 +0200

    new chat command `pushDailyMenza`

---
+++ bot/src/commands/pushDailyMenza.ts
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from "@discordjs/builders"
+
+import { VOC_RequestSended } from "../vocabulary"
+import { UnauthorizedError } from "../errors"
+import { ChatInputCommand } from "../command"
+import { pushDailyMenza } from "../crons"
+
+
+export class PushDailyMenzaßChatCommnad extends ChatInputCommand {
+    name = "pushDailyMenza"
+    description = "Vynucené přidaní denního menu menza"
+    builder = new SlashCommandBuilder()
+
+    async executable(): Promise<void> {
+        if (!this.hasOneOfRoles(["Root", "Moderátor"]))
+            throw new UnauthorizedError()
+
+        await pushDailyMenza(this.client)
+        await this.replySilent(VOC_RequestSended)
+    }
+}
--- bot/src/crons/menzaDaily.ts
+++ bot/src/crons/menzaDaily.ts
@@ -15,54 +15,58 @@ const categoriesTitles: { [key: string]: string } = {
     "Pizza": "**Pizza**  :pizza:",
 }
 
+export async function pushDailyMenza(client: Client) {
+    let dataResponse: MenzaDataResponse | undefined = undefined
+    try {
+        const { data } = await axios.get(MENZA_API as string)
+        dataResponse = {
+            menu: data.menu,
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    if (!dataResponse) {
+        return
+    }
+
+    const mealsByCategories: { [key: string]: MenzaMeal[] } = {}
+    for (const meal of dataResponse.menu) {
+        if (!(meal.category in mealsByCategories)) {
+            mealsByCategories[meal.category] = []
+        }
+
+        mealsByCategories[meal.category].push(meal)
+    }
+
+    let message = ""
+    for (const category in mealsByCategories) {
+        message += `${categoriesTitles[category]}\n`
+
+        for (const meal of mealsByCategories[category]) {
+            let mealName: string = meal.name.replace("PIZZA ", "")
+            message += `> • ${mealName}\n`
+        }
+
+        message += '\n'
+    }
+
+    const menzaChannelID = "1008760191594016829"
+    const guild = client.guilds.cache.get("960452395312234536")
+    const menzaChannel = await guild?.channels.fetch(menzaChannelID)
+    if (menzaChannel) {
+        if (menzaChannel.isTextBased()) {
+            await menzaChannel.send(message)
+        } else {
+            console.log("Error while fetching menza data.")
+        }
+    }
+}
+
 export function menzaDailyMaker(client: Client) {
     return new CronJob(
         "0 0 5 * * MON-FRI",
         async function () {
-            let dataResponse: MenzaDataResponse | undefined = undefined
-            try {
-                const { data } = await axios.get(MENZA_API as string)
-                dataResponse = {
-                    menu: data.menu,
-                }
-            } catch (err) {
-                console.error(err)
-            }
-            if (!dataResponse) {
-                return
-            }
-
-            const mealsByCategories: { [key: string]: MenzaMeal[] } = {}
-            for (const meal of dataResponse.menu) {
-                if (!(meal.category in mealsByCategories)) {
-                    mealsByCategories[meal.category] = []
-                }
-
-                mealsByCategories[meal.category].push(meal)
-            }
-
-            let message = ""
-            for (const category in mealsByCategories) {
-                message += `${categoriesTitles[category]}\n`
-
-                for (const meal of mealsByCategories[category]) {
-                    let mealName: string = meal.name.replace("PIZZA ", "")
-                    message += `> • ${mealName}\n`
-                }
-
-                message += '\n'
-            }
-
-            const menzaChannelID = "1008760191594016829"
-            const guild = client.guilds.cache.get("960452395312234536")
-            const menzaChannel = await guild?.channels.fetch(menzaChannelID)
-            if (menzaChannel) {
-                if (menzaChannel.isTextBased()) {
-                    await menzaChannel.send(message)
-                } else {
-                    console.log("Error while fetching menza data.")
-                }
-            }
+            await pushDailyMenza(client)
         },
         null,
         true,
